3d82eaee615ebccee0f6ac103085e656d9b79713
Isaia Joëlle <nirinaisaiajoelle@gmail.com>
2024-05-09 13:51:49 +0300

    script-java
    sprint 0

---
+++ script.bat
@@ -0,0 +1,21 @@
+@echo off
+@REM Compile Framework
+
+set DESTINATION_DIR="C:\Users\isaia\Documents\S4\MrNaina\frame\test\WEB-INF"
+set LIB_SERVLET="C:\apache-tomcat-9.0.74\apache-tomcat-9.0.74\lib\servlet-api.jar"
+
+if not exist "%DESTINATION_DIR%\lib" (
+    mkdir "%DESTINATION_DIR%\lib"
+)
+
+REM complilation des fichiers java
+cd src
+javac -cp "%LIB_SERVLET%" -d ..\classes *.java
+
+REM rendre les classes en jar
+jar -cf .\framework.jar -C ..\classes .
+
+REM envoyer le jar vers le dossier dans test
+copy .\framework.jar "%DESTINATION_DIR%\lib"
+
+echo Compilation terminée
---
+++ src/FrontController.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("It arrived successfully");
+            out.println("URL:" + request.getRequestURL().toString());
+
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
