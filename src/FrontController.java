bc4c75691835c4df3ecafb02f0e1e6abf9fcc358
Isaia Joëlle <nirinaisaiajoelle@gmail.com>
2025-01-06 20:11:56 +0300

    sprint13
    Gestion de validation

--- src/FrontController.java
+++ src/FrontController.java
@@ -1,12 +1,10 @@
 package servlet;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,9 +22,12 @@ import com.google.gson.Gson;
 
 import java.lang.reflect.*;
 
+import utils.Mapping;
+import utils.Function;
+import utils.ModelView;
+import utils.MySession;
 import utils.*;
 
-@MultipartConfig
 public class FrontController extends HttpServlet {
     private List<String> controllers;
     private HashMap<String, Mapping> map;
@@ -38,7 +39,7 @@ public class FrontController extends HttpServlet {
             this.controllers = new Function().getAllclazzsStringAnnotation(packageToScan, AnnotationController.class);
             this.map = new Function().scanControllersMethods(this.controllers);
         } catch (Exception e) {
-            e.printStackTrace();
+            e.getMessage();
         }
     }
 
@@ -55,76 +56,89 @@ public class FrontController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        String path = new Function().getURIWithoutContextPath(request);
+        response.setContentType("text/html;charset=UTF-8");
 
+        String path = new Function().getURIWithoutContextPath(request);
         if (path.contains("?")) {
-            int index = path.indexOf("?");
-            path = path.substring(0, index);
+            path = path.substring(0, path.indexOf("?"));
         }
 
-        if (map.containsKey(path)) {
-            Mapping m = map.get(path);
-            try {
-                Class<?> clazz = Class.forName(m.getClassName());
-                Method targetMethod = null;
+        // Check if the requested path exists in the map
+        if (!map.containsKey(path)) {
+            out.println("The map contains nothing: 404 NOT FOUND");
+            return;
+        }
 
-                for (Method method : clazz.getDeclaredMethods()) {
-                    if (method.getName().equals(m.getMethodName())) {
-                        targetMethod = method;
-                        break;
-                    }
+        Mapping m = map.get(path);
+        try {
+            Class<?> clazz = Class.forName(m.getClassName());
+            Method targetMethod = null;
+
+            // Find the method in the class that matches the mapping
+            for (Method method : clazz.getDeclaredMethods()) {
+                if (method.getName().equals(m.getMethodName())) {
+                    targetMethod = method;
+                    break;
                 }
-
-                if (targetMethod == null) {
-                    out.println("Méthode non trouvée : " + m.getMethodName());
-                    return;
-                }
-
-                Object controllerInstance = clazz.newInstance();
-
-                Field[] fields = clazz.getDeclaredFields();
-                for (Field field : fields) {
-                    if (field.getType().equals(MySession.class)) {
-                        field.setAccessible(true);
-                        field.set(controllerInstance, new MySession(request.getSession()));
-                    }
-                }
-
-                Object result;
-                if (targetMethod.getParameterCount() == 1 && targetMethod.getParameterTypes()[0] == FilePart.class) {
-                    Map<String, FilePart> fileDataMap = MultipartParser.parseMultipartRequest(request);
-                    FilePart filePart = fileDataMap.get("file"); 
-                    result = targetMethod.invoke(controllerInstance, filePart);
-                } else {
-                    result = targetMethod.invoke(controllerInstance);
-                }
-
-                if (targetMethod.isAnnotationPresent(JsonAnnotation.class)) {
-                    response.setContentType("application/json");
-                    Gson gson = new Gson();
-                    if (result instanceof ModelView) {
-                        ModelView modelView = (ModelView) result;
-                        out.println(gson.toJson(modelView.getData()));
-                    } else {
-                        out.println(gson.toJson(result));
-                    }
-                } else if (result instanceof ModelView) {
-                    ModelView modelView = (ModelView) result;
-                    String destinationUrl = modelView.getUrl();
-                    for (Map.Entry<String, Object> entry : modelView.getData().entrySet()) {
-                        request.setAttribute(entry.getKey(), entry.getValue());
-                    }
-                    RequestDispatcher dispatcher = request.getRequestDispatcher(destinationUrl);
-                    dispatcher.forward(request, response);
-                } else {
-                    out.println("Erreur: Type de retour non géré (ni ModelView ni JSON)");
-                }
-            } catch (Exception e) {
-                out.println("Erreur lors de l'exécution de la méthode : " + e.getMessage());
             }
-        } else {
-            out.println("404 NOT FOUND");
+
+            if (targetMethod == null) {
+                out.println("Méthode non trouvée : " + m.getMethodName());
+                return;
+            }
+
+            Object[] params = Function.getParameterValue(request, targetMethod, ParamAnnotation.class, ParamObjectAnnotation.class);
+            Object controllerInstance = clazz.getDeclaredConstructor().newInstance();
+
+            // Initialize MySession if required by the controller
+            for (Field field : clazz.getDeclaredFields()) {
+                if (field.getType().equals(MySession.class)) {
+                    field.setAccessible(true);
+                    field.set(controllerInstance, new MySession(request.getSession()));
+                }
+            }
+
+            Object result;
+            if (targetMethod.getParameterCount() == 1 && targetMethod.getParameterTypes()[0] == FilePart.class) {
+                Map<String, FilePart> fileDataMap = MultipartParser.parseMultipartRequest(request);
+                FilePart filePart = fileDataMap.get("file");
+                result = targetMethod.invoke(controllerInstance, filePart);
+            } else {
+                result = targetMethod.invoke(controllerInstance, params);
+            }
+
+            handleResponse(result, targetMethod, request, response, out);
+
+        } catch (Exception e) {
+            e.printStackTrace(out);
+            out.println("Erreur lors de l'exécution de la méthode : " + e.getMessage());
         }
     }
 
+    private void handleResponse(Object result, Method targetMethod, HttpServletRequest request,
+                                HttpServletResponse response, PrintWriter out) throws IOException, ServletException {
+        if (targetMethod.isAnnotationPresent(JsonAnnotation.class)) {
+            response.setContentType("application/json");
+            Gson gson = new Gson();
+            if (result instanceof ModelView) {
+                ModelView modelView = (ModelView) result;
+                out.println(gson.toJson(modelView.getData()));
+            } else {
+                out.println(gson.toJson(result));
+            }
+        } else if (result instanceof String) {
+            out.println("Resultat de l'execution de la méthode " + targetMethod.getName() + " est " + result);
+        } else if (result instanceof ModelView) {
+            ModelView modelView = (ModelView) result;
+            String destinationUrl = modelView.getUrl();
+            HashMap<String, Object> data = modelView.getData();
+            for (String key : data.keySet()) {
+                request.setAttribute(key, data.get(key));
+            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher(destinationUrl);
+            dispatcher.forward(request, response);
+        } else {
+            out.println("Le type de retour n'est ni un String ni un ModelView");
+        }
+    }
 }
